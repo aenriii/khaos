ec186498dcddc437e9acb7b83e7cc72ab44fce60
Chloroxite <zubersonzak@hotmail.com>
2025-02-14 18:11:02 -0600

    the work begins

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ use twilight_cache_inmemory::DefaultInMemoryCache;
 use twilight_gateway::{Event, EventTypeFlags, Intents, Shard, ShardId, StreamExt};
 use twilight_http::Client;
 use twilight_model::id::{
-    marker::{GuildMarker, RoleMarker},
+    marker::{ChannelMarker, GuildMarker, MessageMarker, RoleMarker, UserMarker},
     Id,
 };
 
-const GUILD_ID: Id<GuildMarker> = Id::new(1330311796480938056);
-const ROLE_ID: Id<RoleMarker> = Id::new(1330314630567825429);
+const BOT_ID: Id<UserMarker> = Id::new(1336849855863324763);
+const GUILD_ID: Id<GuildMarker> = Id::new(1336845272541827144);
+const ROLE_ID: Id<RoleMarker> = Id::new(1336846660248141975);
 
 // TODO: Accept nominations via a command
 // TODO: Create a poll every two weeks to determine the new leader of the server
@@ -21,12 +22,22 @@ async fn main() -> anyhow::Result<()> {
     let mut shard = Shard::new(
         ShardId::ONE,
         token.clone(),
-        Intents::GUILDS | Intents::GUILD_MEMBERS,
+        Intents::GUILDS
+            | Intents::GUILD_MEMBERS
+            | Intents::GUILD_MESSAGES
+            | Intents::MESSAGE_CONTENT,
     );
 
     let http = Arc::new(Client::new(token));
 
     let cache = DefaultInMemoryCache::builder().build();
+
+    send_message(
+        Arc::clone(&http),
+        "Bot online!",
+        Id::new(1336845273263243296),
+    )
+    .await;
 
     while let Some(msg) = shard.next_event(EventTypeFlags::all()).await {
         let Ok(event) = msg else {
@@ -43,7 +54,28 @@ async fn main() -> anyhow::Result<()> {
 }
 
 async fn handle_event(event: Event, http: Arc<Client>) -> Result<(), Box<dyn Error + Send + Sync>> {
-    println!("DEBUG: {event:?}");
+    match event {
+        Event::MessageCreate(msg) => {
+            println!("MESSAGE: {}", msg.content);
+            if msg.author.id != BOT_ID {
+                http.create_message(msg.channel_id)
+                    .reply(msg.id)
+                    .content(&msg.content)
+                    .await?;
+            }
+        }
+        _ => println!("DEBUG: {event:?}"),
+    }
+
+    Ok(())
+}
+
+async fn send_message(
+    http: Arc<Client>,
+    msg: &str,
+    cid: Id<ChannelMarker>,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    http.create_message(cid).content(&msg).await?;
 
     Ok(())
 }
