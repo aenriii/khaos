e04654b6cec8e3ec0bdf4640bf4496b132641e56
Alexander Hill <ahill@breadpudding.dev>
2025-02-23 21:27:18 -0500

    Added configuration and redis

---
+++ src/config.rs
@@ -0,0 +1,34 @@
+use serde::Deserialize;
+use twilight_model::id::{marker::{GuildMarker, RoleMarker}, Id};
+
+// I did a funny. ~ahill
+#[derive(Clone, Deserialize)]
+pub(crate) struct KhaosControl {
+    guild: Id<GuildMarker>,
+    prefix: String,
+    redis: String,
+    role: Id<RoleMarker>,
+    token: String,
+}
+
+impl KhaosControl {
+    pub fn guild(&self) -> Id<GuildMarker> {
+        self.guild
+    }
+
+    pub fn prefix(&self) -> &String {
+        &self.prefix
+    }
+
+    pub fn redis(&self) -> &String {
+        &self.redis
+    }
+
+    pub fn role(&self) -> Id<RoleMarker> {
+        self.role
+    }
+
+    pub fn token(&self) -> String {
+        self.token.clone()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,57 @@
+use deadpool_redis::{Config, Connection, Runtime};
 use std::{error::Error, fs, sync::Arc};
 use twilight_cache_inmemory::DefaultInMemoryCache;
 use twilight_gateway::{Event, EventTypeFlags, Intents, Shard, ShardId, StreamExt};
 use twilight_http::Client;
 use twilight_model::channel::Message;
 use twilight_model::id::{
-    marker::{ChannelMarker, GuildMarker, MessageMarker, RoleMarker, UserMarker},
+    marker::{ChannelMarker, MessageMarker},
     Id,
 };
 
-const BOT_ID: Id<UserMarker> = Id::new(1336849855863324763);
-const GUILD_ID: Id<GuildMarker> = Id::new(1336845272541827144);
-const ROLE_ID: Id<RoleMarker> = Id::new(1336846660248141975);
+mod config;
+
+use config::KhaosControl;
 
 // TODO: Accept nominations via a command
 // TODO: Create a poll every two weeks to determine the new leader of the server
 // TODO: Prevent the leader from fighting the bot
 
+async fn handle_event(
+    config: KhaosControl,
+    http: Arc<Client>,
+    database: Connection,
+    event: Event,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    match event {
+        Event::MessageCreate(msg) => {
+            println!("MESSAGE: {}", msg.content);
+            parse_command(&config, http, database, &msg).await.unwrap();
+        }
+        _ => println!("DEBUG: {event:?}"),
+    }
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let token = fs::read_to_string(".token")?;
+    let config = toml::from_str::<KhaosControl>(&fs::read_to_string("khaos.toml")?)?;
 
     let mut shard = Shard::new(
         ShardId::ONE,
-        token.clone(),
+        config.token().clone(),
         Intents::GUILDS
             | Intents::GUILD_MEMBERS
             | Intents::GUILD_MESSAGES
             | Intents::MESSAGE_CONTENT,
     );
 
-    let http = Arc::new(Client::new(token));
+    let http = Arc::new(Client::new(config.token()));
 
     let cache = DefaultInMemoryCache::builder().build();
 
-    send_message(
-        Arc::clone(&http),
-        "Bot online!",
-        Id::new(1336845273263243296),
-        None,
-    )
-    .await
-    .unwrap();
+    let pool = Config::from_url(config.redis()).create_pool(Some(Runtime::Tokio1))?;
 
     while let Some(msg) = shard.next_event(EventTypeFlags::all()).await {
         let Ok(event) = msg else {
@@ -48,21 +59,39 @@ async fn main() -> anyhow::Result<()> {
             continue;
         };
 
+        let database = pool.get().await?;
+
         cache.update(&event);
 
-        tokio::spawn(handle_event(event, Arc::clone(&http)));
+        tokio::spawn(handle_event(config.clone(), Arc::clone(&http), database, event));
     }
 
     Ok(())
 }
 
-async fn handle_event(event: Event, http: Arc<Client>) -> Result<(), Box<dyn Error + Send + Sync>> {
-    match event {
-        Event::MessageCreate(msg) => {
-            println!("MESSAGE: {}", msg.content);
-            parse_command(&msg, http).await.unwrap();
+async fn parse_command(
+    config: &KhaosControl,
+    http: Arc<Client>,
+    _database: Connection,
+    msg: &Message,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    if msg.content.starts_with(config.prefix()) {
+        let args: Vec<&str> = msg.content[config.prefix().len()..]
+            .split_whitespace()
+            .collect();
+
+        match args[0] {
+            "nominate" => {
+                // ...
+            }
+            "test" => {
+                send_message(http, "Reply", msg.channel_id, Some(msg.id)).await?;
+            }
+            "say" => {
+                send_message(http, args[1], msg.channel_id, Some(msg.id)).await?;
+            }
+            _ => {}
         }
-        _ => println!("DEBUG: {event:?}"),
     }
 
     Ok(())
@@ -78,27 +107,6 @@ async fn send_message(
         http.create_message(cid).reply(value).content(&msg).await?;
     } else {
         http.create_message(cid).content(&msg).await?;
-    }
-
-    Ok(())
-}
-
-async fn parse_command(
-    msg: &Message,
-    http: Arc<Client>,
-) -> Result<(), Box<dyn Error + Send + Sync>> {
-    let msg_vec: Vec<&str> = msg.content.as_str().split_whitespace().collect();
-
-    match msg_vec[0] {
-        "!test" => {
-            send_message(http, "Reply", msg.channel_id, Some(msg.id)).await?;
-        }
-        "!say" => {
-            send_message(http, msg_vec[1], msg.channel_id, Some(msg.id)).await?;
-        }
-        _ => {
-            println!("DEBUG: No valid command entered");
-        }
     }
 
     Ok(())
